c1a2cc9d2727a8d23b15165fef45c79744dd2c58
linxi <lx02321@163.com>
2019-03-20 00:15:50 +0800

    调整样式

--- src/view/main-header.js
+++ src/view/main-header.js
@@ -1,6 +1,6 @@
 import React,{Component} from "react";
 import {Link} from "react-router-dom"
-import {Layout,Row,Col,Divider,Menu} from "antd"
+import {Layout,Row,Col,Divider,Menu,Icon} from "antd"
 
 class MainHeader extends Component {
   render (){
@@ -12,15 +12,21 @@ class MainHeader extends Component {
           </Col>
           <Col md={18} xs={0}>
             <Divider type="vertical" className="headerDivider"></Divider>
-            <Menu mode="horizontal">
+            <Menu mode="horizontal" id="nav" theme="light">
               <Menu.Item>
-                <Link to="/index">首页</Link>
+                <Link to="/index">
+                  <Icon type="home"/>
+                  首页</Link>
               </Menu.Item>
               <Menu.Item>
-                <Link to="/book">教程</Link>
+                <Link to="/book">
+                  <Icon type="book"/>
+                  教程</Link>
               </Menu.Item>
               <Menu.Item>
-                <Link to="/about">关于</Link>
+                <Link to="/about">
+                  <Icon type="info-circle-o"/>
+                  关于</Link>
               </Menu.Item>
             </Menu>
           </Col>
